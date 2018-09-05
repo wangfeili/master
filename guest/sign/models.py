8972e801cb6844410ff30169dbf81f511621fd79
wangfeili <37933241+wangfeili@users.noreply.github.com>
2018-09-05 22:32:17 +0800

    添加注释

--- guest/sign/models.py
+++ guest/sign/models.py
@@ -14,12 +14,12 @@ class Event(models.Model):
         return self.name
 
 class Guest(models.Model):
-    event = models.ForeignKey(Event)  #
-    realname = models.CharField(max_length=64)
-    phone = models.CharField(max_length=16)
-    email = models.EmailField()
-    sign = models.BooleanField()
-create_time = models.DateTimeField(auto_now=True)
+    event = models.ForeignKey(Event)  #发布会关联id
+    realname = models.CharField(max_length=64)  #姓名
+    phone = models.CharField(max_length=16)  #手机号
+    email = models.EmailField()  #邮箱
+    sign = models.BooleanField()  #签到状态
+create_time = models.DateTimeField(auto_now=True)  #创建时间（自动获取当前时间）
 
 class Meta:
     unique_together = ("event","phone")
