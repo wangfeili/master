b4043aa9340a015f55b9fc72c1db2f0d57e29afe
wangfeili <37933241+wangfeili@users.noreply.github.com>
2018-10-21 13:19:31 +0800

    test Initial commit
    Signed-off-by: wangfeili <37933241+wangfeili@users.noreply.github.com>

---
+++ myproject/myapp/__init__.py
@@ -0,0 +1,1 @@
+from myproject import myapp
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import myapp.urls
+from django.views.generic import TemplateView
+from django.conf.urls import url,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/', include(myapp.urls)),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
 ]
