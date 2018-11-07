72b7c1d6dab0fe26916c946e7660a7b547cf0813
wangfeili <37933241+wangfeili@users.noreply.github.com>
2018-11-07 23:48:06 +0800

    增加前后结合
    test11.7

--- myproject/appfront/src/components/Home.vue
+++ myproject/appfront/src/components/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
-    <el-row display="margin-top:10px">
+    <el-row display="margin-top:20px">
       <el-col :span="4"><el-input v-model="input" placeholder="请输入书名"></el-input></el-col>
-      <el-button type="primary" @click="addBook()">新增</el-button>
+      <el-button type="primary" v-on:click.native="addBook()">新增</el-button>
     </el-row>
     <el-row>
       <el-table :data="bookList" style="width: 100%" border>
@@ -19,3 +19,63 @@
     </el-row>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      input: '',
+      bookList: [],
+    }
+  },
+  mounted: function() {
+      this.showBooks()
+  },
+  methods: {
+    addBook(){
+      this.$http.get('http://127.0.0.1:8000/api/add_book?book_name=' + this.input)
+        .then((response) => {
+            var res = JSON.parse(response.bodyText)
+            if (res.error_num == 0) {
+              this.showBooks()
+            } else {
+              this.$message.error('新增书籍失败，请重试')
+              console.log(res['msg'])
+            }
+        })
+    },
+    showBooks(){
+      this.$http.get('http://127.0.0.1:8000/api/show_books')
+        .then((response) => {
+            var res = JSON.parse(response.bodyText)
+            console.log(res)
+            if (res.error_num == 0) {
+              this.bookList = res['list']
+            } else {
+              this.$message.error('查询书籍失败')
+              console.log(res['msg'])
+            }
+        })
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
